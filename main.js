e30905e07b1f23cde1c1e57b445e9755a9b9235a
ripsp3c <loblakeslee@gmail.com>
2025-03-07 18:36:45 -0600

    Update and rename index.html to main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 900,
+        height: 700,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+    win.loadFile('index.html');
+    // win.webContents.openDevTools(); // Uncomment for debugging
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
